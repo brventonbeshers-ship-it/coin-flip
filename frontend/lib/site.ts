54e3087a4e4fe4968d2c932a6e9c19be424bcf6f
brventonbeshers-ship-it <brventonbeshers@gmail.com>
2026-04-15 13:13:29 +0300

    config: adjust site description annotations

--- frontend/lib/site.ts
+++ frontend/lib/site.ts
@@ -16,3 +16,4 @@ export const SITE_KEYWORDS = ["coin flip", "heads or tails", "stacks game", "on-
 // rep-site-lib: 1776171083512
 // rep-site-lib: 1776186618565
 // rep-site-lib: 1776215652430
+// rep-site-lib: 1776248009181
