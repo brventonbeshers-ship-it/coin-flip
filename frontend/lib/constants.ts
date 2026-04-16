1fcf68b739056e42814d2d67ca0dd3cee0afb019
brventonbeshers-ship-it <brventonbeshers@gmail.com>
2026-04-16 12:24:51 +0300

    chore: annotate constant tuning

--- frontend/lib/constants.ts
+++ frontend/lib/constants.ts
@@ -18,3 +18,4 @@ export const LEADERBOARD_SIZE = 10;
 // rep-constants-lib: 1776256934071
 // rep-constants-lib: 1776270079374
 // rep-constants-lib: 1776316126164
+// rep-constants-lib: 1776331491168
