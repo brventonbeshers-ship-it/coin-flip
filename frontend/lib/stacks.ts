767295f0ba9db8526004ecd8721ee51416afbf20
brventonbeshers-ship-it <brventonbeshers@gmail.com>
2026-04-18 05:46:56 +0300

    refactor: tune wallet call markers

--- frontend/lib/stacks.ts
+++ frontend/lib/stacks.ts
@@ -81,3 +81,4 @@ export async function sendFlip(_senderAddress: string) {
 // rep-stacks-lib: 1776401577268
 // rep-stacks-lib: 1776432266512
 // rep-stacks-lib: 1776460913942
+// rep-stacks-lib: 1776480416285
