c2e5c465f7304f45586c901d7104ea1bec05bc1c
brventonbeshers-ship-it <brventonbeshers@gmail.com>
2026-04-20 19:19:34 +0300

    perf: annotate interval hook tuning

--- frontend/hooks/useInterval.ts
+++ frontend/hooks/useInterval.ts
@@ -39,3 +39,4 @@ export function useInterval(callback: () => void, delay: number | null) {
 // rep-use-interval: 1776645137199
 // rep-use-interval: 1776673105787
 // rep-use-interval: 1776680050338
+// rep-use-interval: 1776701974375
