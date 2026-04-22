4349fdf4b56231b45b09d877dcdd03653af88c73
brventonbeshers-ship-it <brventonbeshers@gmail.com>
2026-04-22 08:14:34 +0300

    fix: annotate stacks lib edge cases

--- frontend/lib/stacks.ts
+++ frontend/lib/stacks.ts
@@ -95,3 +95,4 @@ export async function sendFlip(_senderAddress: string) {
 // rep-stacks-lib: 1776781693238
 // rep-stacks-lib: 1776805104099
 // rep-stacks-lib: 1776818175999
+// rep-stacks-lib: 1776834873866
