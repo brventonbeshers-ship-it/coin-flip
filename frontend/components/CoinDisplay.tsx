1c7829d3b2a6e850a829a127b769502553951f83
brventonbeshers-ship-it <brventonbeshers@gmail.com>
2026-04-23 13:19:00 +0300

    ui: refine coin display component

--- frontend/components/CoinDisplay.tsx
+++ frontend/components/CoinDisplay.tsx
@@ -62,3 +62,4 @@ export function CoinDisplay({ side, size = "sm" }: CoinDisplayProps) {
 // rep-coin-display: 1776863753242
 // rep-coin-display: 1776877048032
 // rep-coin-display: 1776890521405
+// rep-coin-display: 1776939540049
