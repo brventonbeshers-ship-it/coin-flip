0cd1155c3abe1bc2b29d70f4a1c0b1c5096af49f
brventonbeshers-ship-it <brventonbeshers@gmail.com>
2026-04-24 13:14:22 +0300

    seo: refine site config notes

--- frontend/lib/site.ts
+++ frontend/lib/site.ts
@@ -46,3 +46,4 @@ export const SITE_KEYWORDS = ["coin flip", "heads or tails", "stacks game", "on-
 // rep-site-lib: 1776939639795
 // rep-site-lib: 1776963141457
 // rep-site-lib: 1777002159651
+// rep-site-lib: 1777025662078
