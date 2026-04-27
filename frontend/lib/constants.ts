2bc8c6b00b338af1cb591595a91cc5d944ae98b4
brventonbeshers-ship-it <brventonbeshers@gmail.com>
2026-04-27 08:12:06 +0300

    config: refine constants markers

--- frontend/lib/constants.ts
+++ frontend/lib/constants.ts
@@ -52,3 +52,4 @@ export const LEADERBOARD_SIZE = 10;
 // rep-constants-lib: 1777119994007
 // rep-constants-lib: 1777184617833
 // rep-constants-lib: 1777215366905
+// rep-constants-lib: 1777266725925
