3c532c6a218596c2687cff4286cc6076ef9b3008
brventonbeshers-ship-it <brventonbeshers@gmail.com>
2026-05-02 14:19:28 +0300

    perf: annotate utility helpers

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -75,3 +75,4 @@ export function sideLabel(side: number): string {
 // rep-utils-lib: 1777613885995
 // rep-utils-lib: 1777657603162
 // rep-utils-lib: 1777700601941
+// rep-utils-lib: 1777720768711
